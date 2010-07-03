a8416f8ca08ec79e18a5addde67ea9686a2da3cd
Clive Crous <clive@crous.co.za>
2010-07-03 09:48:38 +0200

    Ignore line endings in file comparisons

--- features/steps/file_contents.rb
+++ features/steps/file_contents.rb
@@ -1,6 +1,6 @@
 Then /^the file contents of "([^"]*)" and the "([^"]*)" yaml template should be identical$/ do |filename_a, filename_b|
   yaml_template_filename = File.expand_path( File.join( File.dirname( __FILE__ ), '..', 'fixtures', filename_b + '.yaml' ) )
-  File.read( filename_a ).should == File.read( yaml_template_filename )
+  File.readlines( filename_a ).map{|n|n.chomp}.should == File.readlines( yaml_template_filename ).map{|n|n.chomp}
 end
 
 Then /^the file contents of the temporary file and the "([^"]*)" yaml template should be identical$/ do |filename|
